45e2976f572f845ec5d21536ee5aea1cdc0622b8
Arda Koyuncu <koyuncuarda55@gmail.com>
2023-10-12 12:12:53 +0300

    Add dancenin page entry animation

--- src/Components/DanceVideo.jsx
+++ src/Components/DanceVideo.jsx
@@ -1,62 +1,79 @@
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../assets/css/DanceVideo.css";
-import { useEffect, useRef, useState } from "react";
+import Zoom from "react-reveal/Zoom";
+import danceVid from "../assets/videos/dancing.mp4";
+import { Animator, ScrollContainer, ScrollPage } from "react-scroll-motion";
 
 const DanceVideo = () => {
-      useEffect(() => {
-          const vh = window.innerHeight / 100;
+  const [opacity, setOpacity] = useState(1);
 
-          const handleScroll = () => {
-            const scrollTop = document.documentElement.scrollTop;
-            const start = 100 * vh;
-            const stop = 200 * vh;
-            const zoomElement = document.querySelector('.zoom2');
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const vh = window.innerHeight;
 
-            if (scrollTop > start && scrollTop < stop && zoomElement) {
-              const scale = Math.max(1 + (scrollTop - start) / 500, 1);
-              zoomElement.style.transform = `scale(${scale})`;
-            }
-          };
+      // Belirli bir scroll pozisyonu belirlemek için burada istediğiniz değeri kullanabilirsiniz.
+      const scrollThreshold = 2.1 * vh; // Örneğin, 80vh'de kaybolacaksa
 
-          window.addEventListener('scroll', handleScroll);
-
-          return () => {
-            window.removeEventListener('scroll', handleScroll);
-          };
-        }, []);
-
-    const [scale, setScale] = useState(1);
-
-    useEffect(() => {
-      function handleScroll() {
-        const vh = window.innerHeight / 100;
-        const scrollTop = document.documentElement.scrollTop;
-        const start = 100 * vh;
-        const stop = 150 * vh;
-
-        if (scrollTop > start && scrollTop < stop) {
-          const newScale = Math.max(2.2 - (scrollTop - start) / 500, 1);
-          setScale(newScale);
-        }
+      // Scroll pozisyonu scrollThreshold'dan büyükse opacity'yi azalt
+      if (scrollY > scrollThreshold) {
+        const opacityValue = 1 - (scrollY - scrollThreshold) / (vh * 0.7);
+        setOpacity(opacityValue > 0 ? opacityValue : 0);
+      } else {
+        setOpacity(1); // Eğer scroll pozisyonu threshold'dan küçükse opacity'yi sıfırla
       }
+    };
 
-      window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, []);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
+  const [opacityVid, setOpacityVid] = useState(1);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const vh = window.innerHeight;
+
+      // Belirli bir scroll pozisyonu belirlemek için burada istediğiniz değeri kullanabilirsiniz.
+      const scrollThreshold = 1.5 * vh; // Örneğin, 80vh'de kaybolacaksa
+
+      // Scroll pozisyonu scrollThreshold'dan büyükse opacity'yi azalt
+      if (scrollY > scrollThreshold) {
+        const opacityValue = 1 - (scrollY - scrollThreshold) / (vh * 0.7);
+        setOpacityVid(opacityValue > 0 ? opacityValue : 0);
+      } else {
+        setOpacityVid(1); // Eğer scroll pozisyonu threshold'dan küçükse opacity'yi sıfırla
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
-    <div className="container">
-      <div className="col daceHeader zoom2" style={{scale}}>
-        <div className="row text-center headerOne">Adaptive Audio.</div>
-        <div className="row text-center haderTwo">Now Playing.</div>
-        <div></div>
-      </div>
+    <div class="bg-video-wrap d-flex justify-content-center align-items-center">
+      <video style={{ opacityVid }} src={danceVid} autoPlay loop muted></video>
+      <div class="overlay"></div>
+      <h1 style={{ opacity }}>
+        <Zoom>
+          <div className="daceHeader zoom2">
+            <div className="headerOne">Adaptive Audio.</div>
+            <div className="haderTwo">Now Playing.</div>
+            <div></div>
+          </div>
+        </Zoom>
+      </h1>
+
     </div>
   );
 };
+
 export default DanceVideo;
